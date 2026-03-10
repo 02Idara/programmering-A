9e31faab6e1aee16db6e9863f37f68058b3e8819
Ida <aspel25idar@axevalla.fhsk.se>
2026-03-10 11:27:35 +0100

    o

--- Klara uppgifter/07upp/07upp/Program.cs
+++ Klara uppgifter/07upp/07upp/Program.cs
@@ -16,30 +16,34 @@ namespace _07upp
             string playername = "";
             int playerHP = 100;
             string weaponType = "";
-            string weaponChoice;
+            string weaponChoice = "";
             
             int pMax = 0;
             int pMin = 0;
 
             int enemyHP = 100;
-            int eMaxDamage = 10;
-            int eMinDamage = 5;
-            int enemyDamage;
+            int eMaxDamage = 14;
+            int eMinDamage = 6;
+            
 
             Console.WriteLine("Välkommen! Spelet går till såhär: Du möter en enemy med samma HP som dig (100)\nSedan kommer du få välja ett vapen med olika max & min damage som också kommer att slumpas\nvilken damage du kommer göra.");   
             Console.WriteLine(" ");
             Thread.Sleep(2000);
             Console.WriteLine("Skriv in ditt namn");
             playername = Console.ReadLine();
-            Console.WriteLine("Hej" + playername + "Nu kan spelet börja.");
+            Console.WriteLine("Hej " + playername + " Nu kan spelet börja.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Din HP är " + playerHP);
+            Console.WriteLine("Enemys HP är " + enemyHP);
+            Thread.Sleep(2000);
 
             while (playerHP > 0 && enemyHP > 0)
             {
-                Console.WriteLine("Din HP är " + playerHP);
-                Console.WriteLine("Enemys HP är " + enemyHP);
 
-                while (weaponType == "")
+
+                while (weaponChoice == "")
                 {
+                    Console.WriteLine(" ");
                     Console.WriteLine("Välj ett utav vapenen");
                     Console.WriteLine("sword = Maxdamage=12 Mindamage=10  tryck 1\n axe = Maxdamage=14 Mindamage=8  tryck 2\nhammer = Maxdamage=16 Mindamage=4  tryck 3");
                     weaponChoice = Console.ReadKey().KeyChar.ToString();
@@ -59,11 +63,34 @@ namespace _07upp
                             break;
                             
                     }
+
                 }
-                int playerdamage = rnd.Next(pMax, pMin);
+                int playerdamage = rnd.Next(pMin, pMax);
+                int enemyDamage = rnd.Next(eMinDamage, eMaxDamage);
 
-                
+                playerHP = playerHP - enemyDamage;
+                enemyHP = enemyHP - playerdamage;
+                Console.WriteLine(" ");
+                Console.WriteLine("Du gjorde " + playerdamage + " i skada");
+                Console.WriteLine("Enemy gjorde " + enemyDamage + " i skada");
+                Console.WriteLine(" ");
+                Console.WriteLine("Din HP är " + playerHP);
+                Console.WriteLine("Enemys HP är " + enemyHP);
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                weaponChoice = "";
 
+
+
+
+            }
+            if (playerHP > 0)
+            {
+                Console.WriteLine("Grattis du har vunnit!!!");
+            }
+            else
+            {
+                Console.WriteLine("Du har förlorat");
             }
         }
     }

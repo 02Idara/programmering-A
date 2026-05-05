ba17ef83c2e14c546606250a87d4a50434d54d68
Ida <aspel25idar@axevalla.fhsk.se>
2026-05-05 11:57:18 +0200

    upp 10

---
+++ oklara uppgifter/upp 10/upp 10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upp_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkomen till Enarmad bandit");
+            Console.WriteLine("Spelet funkar såhär:\nDu satsar pengar ");
+        }
+    }
+}
